059946e2ab83653442de0f7269e856c833ac5401
Thibault Dockx <thibault.dockx@fusiondirectory.org>
2023-02-10 12:04:44 +0000

    :sparkles: Feat(PluginsManager) - Starting from clean state.
    Restarting development from clean state.

--- src/FusionDirectory/Tools/PluginsManager.php
+++ src/FusionDirectory/Tools/PluginsManager.php
@@ -24,7 +24,7 @@ namespace FusionDirectory\Tools;
 use \FusionDirectory\Cli;
 use \FusionDirectory\Ldap;
 
-class InsertSchema extends Cli\Application
+class PluginsManager extends Cli\Application
 {
   /**
    * @var Ldap\Link
@@ -34,6 +34,11 @@ class InsertSchema extends Cli\Application
   public function __construct ()
   {
     parent::__construct();
+
+    // List mandatory options and available actions 
+    /* set-fd_home=FD PATH' : path of fusiondirectory installtion */
+    /* plugins-archive=SRC_PATH : path of directory ( or gz archive) of plugins to scan */
+    /* plugin-name=plugin name : name of plugin ( contain in control.yaml file) */
 
     $this->options  = [
       'register-plugin'  => [
@@ -125,128 +130,19 @@ class InsertSchema extends Cli\Application
   }
 
   // function that add plugin record
-  function addPluginRecord() {
+  public function addPluginRecord() {
 
     // Load the information from the yaml file
-    $pluginInfo = loadFile($pathfile);
 
     // Verify if the branch for plugins already exist and create it if not.
-    if (!branchExists($ldap, "$configpluginrdn,ou=fusiondirectory,$base")) {
-      createBranch($ldap, "ou=fusiondirectory,".$base, $configpluginrdn);
-      print "Create plugin branch";
-    }
-
-    $obj=['objectClass' => ['top','fdPlugin']];
 
     // Collect and arrange the info received by the yaml file.
-    foreach (array_keys($pluginmanagementmapping) as $k) {
-      $section = preg_split('/:/', $pluginmanagementmapping[$k]);
-      if (/*check*/isset($pluginInfo[$section[0]][$section[1]])){
-        $obj[$k] = $pluginInfo[$section[0]][$section[1]];
-      }
-    }
-    // Create the proper DN
+    
+    // Create the proper CN
     $dn = "cn=".$pluginInfo['information']['name'].",".$configpluginrdn.",ou=fusiondirectory,".$base;
 
-    $options = $obj;
-
     // Verifying if the record of the plugin already exists and delete it.
-    if (branchExists($ldap, $dn)) {
-      print "Plugin record exist : ".$dn."\nDeleting it !\n";
-      deletePluginRecord($dn);
-    }
-
     // Create the record for the plugin.
-    $mesg = $ldap->add( $dn, 'attr' => /*check:\*/$options );
-    print "Create plugin record\n";
-    if ($mesg->code) {
-      print $dn.": ".$mesg->error." (".$mesg->code.")\n";
-    }
   }
 
-  /* // function that delete plugin record */
-  /* function deletePluginRecord() { */
-  /*   // initiate the LDAP connexion */
-  /*   $pluginDn=$fake/*check:$*/[0]; */
-  /*   $hashLdapParam = getLdapConnexion(); */
-
-  /*   // LDAP's connection's parameters */
-  /*   $base = $hashLdapParam['base']; */
-  /*   $ldap = $hashLdapParam['ldap']; */
-
-  /*   if (!branchExists($ldap,$pluginDn)) { */
-  /*     exit; */
-  /*   }else{ */
-  /*     $mesg = $ldap->unset($pluginDn); */
-
-  /*     if ($mesg->code) { */
-  /*       print $pluginDn.": ".$mesg->error."\n"; */
-  /*     } */
-  /*   } */
-  /* } */
-
-  /* // function that check if plugin is inserted ldap tree */
-  /* function checkPluginExistence() { */
-
-  /*   // check if plugin is set on CLI */
-  /*   $pluginName = $fake/*check:$*/[0]; */
-
-  /*   // initiate the LDAP connexion */
-  /*   $hashLdapParam = getLdapConnexion(); */
-
-  /*   // LDAP's connection's parameters */
-  /*   $base = $hashLdapParam['base']; */
-  /*   $ldap = $hashLdapParam['ldap']; */
-
-  /*   // Search for plugin */
-  /*   $mesg = $ldap->search( */
-  /*     'base' => "$configpluginrdn,ou=fusiondirectory,$base", */
-  /*     'filter' => "(&(objectClass=fdPlugin)(cn=".$pluginName."))", */
-  /*     'attrs' => ['cn','description'] */
-  /*   ); */
-  /*   $mesg->code && die $mesg->error; */
-  /*   $entries = $mesg->entries; */
-
-  /* //  print($mesg->code."  ".$mesg->error." ".ref($mesg->code)); */
-  /*   if ($mesg->count == 1){ */
-  /*     print("Plugin ".$pluginName." is declared\n"); */
-  /*     return(1); */
-  /*   }else{ */
-  /*     print("Plugin ".$pluginName." is NOT declared\n"); */
-  /*     return(0); */
-  /*   } */
-  /* } */
-
-  /* function listPlugins() { */
-  /*   // initiate the LDAP connexion */
-  /*   $hashLdapParam = getLdapConnexion(); */
-
-  /*   // LDAP's connection's parameters */
-  /*   $base = $hashLdapParam['base']; */
-  /*   $ldap = $hashLdapParam['ldap']; */
-
-  /*   $pluginattrs=['cn','description','fdPluginInfoAuthors','fdPluginInfoVersion','fdPluginSupportHomeUrl','fdPluginInfoStatus','fdPluginSupportProvider','fdPluginInfoOrigin']; */
-
-  /*   // Search for DHCP configurations */
-  /*   $mesg = $ldap->search( */
-  /*     'base' => "$configpluginrdn,ou=fusiondirectory,$base", */
-  /*     'filter' => "(objectClass=fdPlugin)", */
-  /*     'attrs' => /*check:\*/$pluginattrs */
-  /*   ); */
-  /*   $mesg->code && die $mesg->error; */
-
-  /*   $entries = $mesg->entries; */
-  /*   print "There are ".$mesg->count." Plugins configurations in the LDAP\n"; */
-
-  /*   foreach ($entries as $entry) { */
-  /*     print " Plugin :".$entry->getValue('cn')."\n"; */
-  /*     foreach ($pluginattrs as $val) { */
-  /*       $section = preg_split('/:/', $pluginmanagementmapping[$val]); */
-  /*       $value="N/A"; */
-  /*       if (/*check*/isset(($entry->getValue($val)))){ */
-  /*           $value = $entry->getValue($val); */
-  /*       } */
-  /*       print "   - ".$section[1]."\t: ".$value."\n"; */
-  /*     } */
-  /*   } */
-}
+} 
